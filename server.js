33fc23e8e603053686cda3369a84075cfa20efc8
Pratham <prathamesh.sahasrabhojane@getsimpl.com>
2023-08-17 18:50:21 +0530

    change port

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use('/', elbController);
 app.use('/api/v1', wishlistController);
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
